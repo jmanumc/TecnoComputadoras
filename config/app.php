c8bf02f05423793cf3030f168fc7542695332378
Juan Manuel Martínez <j-manuel_mtz23@hotmail.com>
2016-01-09 13:12:32 -0600

    Integración de Laravel Collective

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * External Service Providers
+         */
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -202,6 +207,11 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+        /*
+         * External Alises
+         */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
