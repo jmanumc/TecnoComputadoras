7b975cc8f1c2fa1afeb146a29325ba58048f94d9
Juan Manuel Martínez <j-manuel_mtz23@hotmail.com>
2016-01-09 21:48:08 -0600

    Modulo Edit de Users creado

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Requests\UserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Http\Controllers\Controller;
 
 use App\User;
@@ -39,12 +40,65 @@ class UsersController extends Controller
     	return redirect()->route('admin.users.index');
     }
 
+    public function edit ($id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('admin.users.edit')->with('user', $user);
+    }
+
+    public function update(UpdateUserRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $changes = false;
+
+        if($user->name != $request->name) {
+            $user->name = $request->name;
+            $changes = true;
+        }
+
+        if($user->email != $request->email) {
+            $user->email = $request->email;
+            $changes = true;
+        }
+
+        if($user->type != $request->type) {
+            $user->type = $request->type;
+            $changes = true;
+        }
+
+        if($request->has('password')) {
+            $user->password = $request->password;
+            $changes = true;
+        }
+
+        if(!empty($request->file('avatar'))) {
+            $avatar_old = $user->avatar;
+            print($avatar_old);
+            $user->avatar = $this->manipulateImage($request->file('avatar'));
+
+            if($avatar_old != 'user-avatar-default.png' && Storage::disk('local')->exists('users/avatars/' . $avatar_old)) {
+                Storage::delete('users/avatars/' . $avatar_old);
+            }
+            $changes = true;
+        }
+
+        if($changes) {
+            $user->save();
+
+            Flash::success('El usuario ' . $user->name . ' ha sido actualizado exitosamente');
+            return redirect()->route('admin.users.index');
+        } else {
+            return redirect()->back();
+        }
+    }
+
     public function manipulateImage($image)
     {
-		$name = 'avatar_' . microtime(true) . '.' . $image->getClientOriginalExtension();
-		$path = 'users/avatars/' . $name;
-		Storage::put($path, file_get_contents($image));
+        $name = 'avatar_' . microtime(true) . '.' . $image->getClientOriginalExtension();
+        $path = 'users/avatars/' . $name;
+        Storage::put($path, file_get_contents($image));
 
-		return $name;
+        return $name;
     }
 }
---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class UpdateUserRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'                  => 'min:3|required',
+            'email'                 => 'email|required',
+            'password'              => 'same:password_confirmation|min:4',
+            'type'                  => 'in:admin,member|required',
+            'avatar'                => 'image'
+        ];
+    }
+}
