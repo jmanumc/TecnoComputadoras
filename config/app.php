63f3a214d1297254cbe64c1b4da210dce24980f2
Juan Manuel Martínez <j-manuel_mtz23@hotmail.com>
2016-01-10 12:21:23 -0600

    Implementado dwightwatson Active en el proyecto

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
          */
         Collective\Html\HtmlServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
+        Watson\Active\ActiveServiceProvider::class,
 
     ],
 
@@ -214,6 +215,7 @@ return [
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
         'Flash'     => Laracasts\Flash\Flash::class,
+        'Active'    => Watson\Active\Facades\Active::class,
     ],
 
 ];
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,4 +1,6 @@
 <ul class="nav nav-sidebar">
 	<li><a href="#"><i class="fa fa-home fa-btn"></i>Inicio</a></li>
-	<li class="active"><a href="{{ route('admin.users.index') }}"><i class="fa fa-user fa-btn"></i>Usuarios</a></li>
+	<li class="{{ active('admin.users.index') }}">
+		<a href="{{ route('admin.users.index') }}"><i class="fa fa-user fa-btn"></i>Usuarios</a>
+	</li>
 </ul>
