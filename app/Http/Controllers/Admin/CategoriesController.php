d1ec31a976013c023b3c088ae1d24c10feaa2945
Juan Manuel Martínez <j-manuel_mtz23@hotmail.com>
2016-01-17 20:29:40 -0600

    Añadido el link de las categorias a el sidebar y Implementando el middleware auth

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -14,6 +14,11 @@ use Laracasts\Flash\Flash;
 
 class CategoriesController extends Controller
 {
+	public function __construct ()
+	{
+		$this->middleware('auth');
+	}
+
 	public function index ()
 	{
 		$categories = Category::orderBy('id', 'DESC')->paginate(10);
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -3,4 +3,7 @@
 	<li class="{{ active('admin.users.index') }}">
 		<a href="{{ route('admin.users.index') }}"><i class="fa fa-user fa-btn"></i>Usuarios</a>
 	</li>
+	<li class="{{ active('admin.categories.index') }}">
+		<a href="{{ route('admin.categories.index') }}"><i class="fa fa-home fa-btn"></i>Categorias</a>
+	</li>
 </ul>
